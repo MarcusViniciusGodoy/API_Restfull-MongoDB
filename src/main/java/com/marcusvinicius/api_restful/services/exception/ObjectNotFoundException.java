9feb439881d75f04f66d495196d75bc72637ed9c
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2024-01-03 18:22:30 -0300

    ResourceException and UserService

---
+++ src/main/java/com/marcusvinicius/api_restful/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.marcusvinicius.api_restful.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
