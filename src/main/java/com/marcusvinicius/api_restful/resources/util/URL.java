6d6e7b3499df976503630b825112effcb643d545
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2024-01-06 17:26:41 -0300

    minDate and maxDate

--- src/main/java/com/marcusvinicius/api_restful/resources/util/URL.java
+++ src/main/java/com/marcusvinicius/api_restful/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.marcusvinicius.api_restful.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -10,6 +14,16 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return (Date) sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
 		}
 	}
 }
